451c47320542080ce2afbf838c89ff66696cdf0a
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-07 18:21:38 +0700

    membuat implementasi AppContainer menggunakan Firebase Firestore untuk menyediakan AnggotaRepository

--- app/src/main/java/com/example/perpustakaan/data/PerpusContainer.kt
+++ app/src/main/java/com/example/perpustakaan/data/PerpusContainer.kt
@@ -1,5 +1,15 @@
 package com.example.perpustakaan.data
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 interface AppContainer {
     val anggotaRepository: AnggotaRepository
+}
+
+class PerpusContainer : AppContainer {
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    override val anggotaRepository: AnggotaRepository by lazy {
+        AnggotaRepositoryImpl(firestore)
+    }
 }
