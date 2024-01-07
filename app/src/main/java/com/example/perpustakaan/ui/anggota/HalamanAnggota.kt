d35ff87a1838eb5cced3099de97cadbd82caed5d
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-07 23:06:21 +0700

    membuat function DataAnggota dan parameter nya

--- app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
@@ -3,6 +3,7 @@ package com.example.perpustakaan.ui.anggota
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
+import com.example.perpustakaan.model.Anggota
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi {
@@ -15,4 +16,10 @@ fun AnggotaScreen(
     navigateToItemEntryAnggota: () -> Unit,
     modifier: Modifier = Modifier,
     onDetailClick: (String) -> Unit = {},
+) {}
+
+@Composable
+fun DataAnggota(
+    anggota: Anggota,
+    modifier: Modifier = Modifier
 ) {}
