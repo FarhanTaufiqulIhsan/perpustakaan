b5d6258ee927531d8f4361daffc418649e1eb0fd
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-07 23:25:54 +0700

    membuat function DataAnggota untuk mendesain bentuk Card yang akan ditampilkan

--- app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
@@ -1,7 +1,22 @@
 package com.example.perpustakaan.ui.anggota
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.example.perpustakaan.model.Anggota
 import com.example.perpustakaan.navigation.DestinasiNavigasi
@@ -22,4 +37,50 @@ fun AnggotaScreen(
 fun DataAnggota(
     anggota: Anggota,
     modifier: Modifier = Modifier
-) {}
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = anggota.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = anggota.nohp,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = anggota.jk,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null,
+                )
+                Text(
+                    text = anggota.alamat,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+    }
+}
