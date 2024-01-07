6d30e1c40de7c0293b077b425e05a3d27755ffeb
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-07 21:44:16 +0700

    Menambahkan object DestinasiHome pada HalamanBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
@@ -1,2 +1,8 @@
 package com.example.perpustakaan.ui.buku
 
+import com.example.perpustakaan.navigation.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Buku"
+}
