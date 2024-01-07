f5c3089191177ec0866654a99f21963f1c5466f0
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-07 18:38:58 +0700

    membuat function AnggotaScreen

--- app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
@@ -1,8 +1,18 @@
 package com.example.perpustakaan.ui.anggota
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Anggota"
 }
+
+@Composable
+fun AnggotaScreen(
+    navigateToItemEntryAnggota: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+) {}
