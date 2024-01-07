419df2c99464cfa42335e0d1f7976f86958370d8
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-07 18:31:26 +0700

    membuat object DestinasiHome yang mengimplementasikan DestinasiNavigasi

---
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.anggota
+
+import com.example.perpustakaan.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Anggota"
+}
