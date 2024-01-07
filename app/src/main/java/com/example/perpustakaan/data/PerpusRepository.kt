ab05219a5169792d04fbdca17e333eaa7cad0175
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-07 18:05:38 +0700

    membuat class AnggotaRepositoryImpl

--- app/src/main/java/com/example/perpustakaan/data/PerpusRepository.kt
+++ app/src/main/java/com/example/perpustakaan/data/PerpusRepository.kt
@@ -1,6 +1,8 @@
 package com.example.perpustakaan.data
 
 import com.example.perpustakaan.model.Anggota
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreKtxRegistrar
 import kotlinx.coroutines.flow.Flow
 
 interface AnggotaRepository {
@@ -9,4 +11,27 @@ interface AnggotaRepository {
     suspend fun update(anggota: Anggota)
     suspend fun delete(anggotaId: String)
     fun getAnggotaById(anggotaId: String): Flow<Anggota>
+}
+
+class AnggotaRepositoryImpl(private val firestore: FirebaseFirestore) : AnggotaRepository {
+    override fun getAll(): Flow<List<Anggota>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun save(anggota: Anggota): String {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(anggota: Anggota) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(anggotaId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAnggotaById(anggotaId: String): Flow<Anggota> {
+        TODO("Not yet implemented")
+    }
+
 }
