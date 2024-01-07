2033611085e9a77a15b21fa6ab37dfc8cea2b5a2
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 00:13:33 +0700

    membuat Data class AddEvent

--- app/src/main/java/com/example/perpustakaan/ui/UiState.kt
+++ app/src/main/java/com/example/perpustakaan/ui/UiState.kt
@@ -6,3 +6,11 @@ data class HomeUIState(
     val listAnggota: List<Anggota> = listOf(),
     val dataLength: Int = 0
 )
+
+data class AddEvent(
+    val id: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jk: String = "",
+    val nohp: String = ""
+)
