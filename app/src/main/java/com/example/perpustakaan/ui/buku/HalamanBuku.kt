e72d5a95fe1929ae9a4d13a662323a09a81c3f60
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-07 21:45:39 +0700

    Menambahkan fun BukuScreen dan paramnya pada file HalamanBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
@@ -1,8 +1,17 @@
 package com.example.perpustakaan.ui.buku
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
 object DestinasiHome: DestinasiNavigasi{
     override val route = "home"
     override val titleRes = "Buku"
 }
+
+@Composable
+fun BukuScreen(
+    navigateToItemEntryBuku: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+) {}
