79ead7faf909f96739154548f44f8c9f76b5c93c
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 17:31:33 +0700

    menambahkan StateFlow uiStateAnggota untuk data Anggota berdasarkan ID, dengan pemetaan ke DetailUIStateAnggota dan stateIn 5,000L

--- app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggotaViewModel.kt
@@ -2,7 +2,15 @@ package com.example.perpustakaan.ui.anggota.detailAnggota
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perpustakaan.data.AnggotaRepository
+import com.example.perpustakaan.ui.DetailUIStateAnggota
+import com.example.perpustakaan.ui.toDetailAnggota
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailAnggotaViewModel(
     savedStateHandle: SavedStateHandle,
@@ -13,4 +21,15 @@ class DetailAnggotaViewModel(
     }
 
     val anggotaId: String = checkNotNull(savedStateHandle[DetailDestinationAnggota.anggotaId])
+
+    val uiStateAnggota: StateFlow<DetailUIStateAnggota> =
+        repository.getAnggotaById(anggotaId)
+            .filterNotNull()
+            .map {
+                DetailUIStateAnggota(addEventAnggota = it.toDetailAnggota())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIStateAnggota()
+            )
 }
