511d9e56a64bab6ce50bc6f7a66119fc9107c27b
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 14:57:58 +0700

    Membuat function FormInputBuku dan parameternya

--- app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBuku.kt
@@ -1,8 +1,19 @@
 package com.example.perpustakaan.ui.buku.addBuku
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.navigation.DestinasiNavigasi
+import com.example.perpustakaan.ui.AddEventBuku
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Buku"
 }
+
+@Composable
+fun FormInputBuku(
+    addEventBuku: AddEventBuku,
+    modifier: Modifier = Modifier,
+    onValueChangeBuku: (AddEventBuku) -> Unit = {},
+    enabled: Boolean = true
+) {}
