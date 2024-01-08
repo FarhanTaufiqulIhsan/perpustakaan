6def21a470203adbdd5dfd68e415c3088cd13f85
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 14:05:22 +0700

    menambahkan function untuk memperbarui state UI AddAnggotaViewModel dengan AddEventAnggota baru

--- app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggotaViewModel.kt
@@ -5,9 +5,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.perpustakaan.data.AnggotaRepository
+import com.example.perpustakaan.ui.AddEventAnggota
 import com.example.perpustakaan.ui.AddUIStateAnggota
 
 class AddAnggotaViewModel(private val anggotaRepository: AnggotaRepository) : ViewModel() {
     var addUIStateAnggota by mutableStateOf(AddUIStateAnggota())
         private set
+
+    fun updateAddUIStateAnggota(addEventAnggota: AddEventAnggota) {
+        addUIStateAnggota = AddUIStateAnggota(addEventAnggota = addEventAnggota)
+    }
 }
