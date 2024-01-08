f86517f648f1ef8359311bee7b77b28061c22ce0
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 15:22:39 +0700

    Membuat fungsi EntryBodyBuku dan parameternya

--- app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBuku.kt
@@ -13,11 +13,20 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 import com.example.perpustakaan.ui.AddEventBuku
+import com.example.perpustakaan.ui.AddUIStateBuku
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Buku"
 }
+
+@Composable
+fun EntryBodyBuku(
+    addUIStateBuku: AddUIStateBuku,
+    onBukuValueChange: (AddEventBuku) -> Unit,
+    onSaveClickBuku: () -> Unit,
+    modifier: Modifier = Modifier
+) {}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
