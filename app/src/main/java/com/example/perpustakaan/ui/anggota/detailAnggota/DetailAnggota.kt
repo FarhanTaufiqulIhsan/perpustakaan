b49c67bea95352e3a7156d17baca5905460afa1b
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 18:52:30 +0700

    membuat Card untuk menampilkan detail Anggota dalam Column dengan ItemDetailsRowAnggota untuk setiap atribut

--- app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggota.kt
@@ -1,13 +1,21 @@
 package com.example.perpustakaan.ui.anggota.detailAnggota
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.model.Anggota
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
@@ -22,7 +30,54 @@ object DetailDestinationAnggota : DestinasiNavigasi {
 fun ItemDetails(
     anggota: Anggota,
     modifier: Modifier = Modifier
-) {}
+) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            ItemDetailsRowAnggota(
+                labelResIDAnggota = "Nama",
+                itemDetailAnggota = anggota.nama,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+
+            ItemDetailsRowAnggota(
+                labelResIDAnggota = "Alamat",
+                itemDetailAnggota = anggota.alamat,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+
+            ItemDetailsRowAnggota(
+                labelResIDAnggota = "Jenis Kelamin",
+                itemDetailAnggota = anggota.jk,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+
+            ItemDetailsRowAnggota(
+                labelResIDAnggota = "Nomor Telepon",
+                itemDetailAnggota = anggota.nohp,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+        }
+    }
+}
 
 @Composable
 private fun ItemDetailsRowAnggota(
