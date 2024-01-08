730f4030965cb64769da7924f143e00aa386e806
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 14:04:03 +0700

    membuat class AddAnggotaViewModel dengan repository AnggotaRepository dan mutableStateOf untuk state UI AddUIStateAnggota

--- app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggotaViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.perpustakaan.ui.anggota.addAnggota
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.perpustakaan.data.AnggotaRepository
+import com.example.perpustakaan.ui.AddUIStateAnggota
+
+class AddAnggotaViewModel(private val anggotaRepository: AnggotaRepository) : ViewModel() {
+    var addUIStateAnggota by mutableStateOf(AddUIStateAnggota())
+        private set
+}
