3c7cc21b1c01ccd608fce7b9e27b50d4a42507c2
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 12:37:15 +0700

    Menambahkan card pada fun DataBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
@@ -1,7 +1,10 @@
 package com.example.perpustakaan.ui.buku
 
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.model.Anggota
 import com.example.perpustakaan.model.Buku
 import com.example.perpustakaan.navigation.DestinasiNavigasi
@@ -23,5 +26,10 @@ fun DataBuku(
     buku: Buku,
     modifier: Modifier = Modifier
 ){
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
 
+    }
 }
