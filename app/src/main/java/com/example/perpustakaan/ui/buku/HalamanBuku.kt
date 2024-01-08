5a5547427137ed87d8e37fcd2fd28f661d7b8271
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 12:40:03 +0700

    Menambahkan column, row, dan text judul

--- app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
@@ -1,7 +1,14 @@
 package com.example.perpustakaan.ui.buku
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -30,6 +37,17 @@ fun DataBuku(
         modifier = modifier,
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
-
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text = buku.judul,
+                    style = MaterialTheme.typography.titleLarge)
+            }
+        }
     }
 }
