3ec06c063e59499b464c2640c1c83cfadd363da2
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 13:01:32 +0700

    Menambahkan Icon date dan text tahun terbit

--- app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -70,7 +71,15 @@ fun DataBuku(
                     text = buku.kategori,
                     style = MaterialTheme.typography.titleLarge,
                     )
-
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = null
+                )
+                Text(
+                    text = buku.tahunterbit,
+                    style = MaterialTheme.typography.titleMedium
+                )
             }
         }
     }
