5d3f99ebadcb152af87bf0871b45e4f0f07a07c0
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 14:43:41 +0700

    Membuat class AddBukuViewModel

--- app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBukuViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBukuViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.perpustakaan.ui.buku.addBuku
 
+import androidx.lifecycle.ViewModel
+import com.example.perpustakaan.data.BukuRepository
+
+class AddBukuViewModel(private val bukuRepository: BukuRepository) : ViewModel() {
+
+}
