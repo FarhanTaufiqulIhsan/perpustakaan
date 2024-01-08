c49558f69ffb998d2317ffab17b33b8661191e4e
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 14:08:13 +0700

    membuat function FormInputAnggota dan parameter nya

--- app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggota.kt
@@ -1,8 +1,19 @@
 package com.example.perpustakaan.ui.anggota.addAnggota
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.navigation.DestinasiNavigasi
+import com.example.perpustakaan.ui.AddEventAnggota
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Anggota"
 }
+
+@Composable
+fun FormInputAnggota(
+    addEventAnggota: AddEventAnggota,
+    modifier: Modifier = Modifier,
+    onValueChange: (AddEventAnggota) -> Unit = {},
+    enabled: Boolean = true
+) {}
