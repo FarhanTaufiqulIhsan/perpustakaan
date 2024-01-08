ef848a9c9b54cb4606c0f4e7ff1490e14156174e
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 15:33:21 +0700

    Membuat Scaffold dengan EntryBodyBuku untuk input buku dan tombol submit, serta menambahlan parameter

--- app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBuku.kt
@@ -4,21 +4,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 import com.example.perpustakaan.ui.AddEventBuku
 import com.example.perpustakaan.ui.AddUIStateBuku
+import com.example.perpustakaan.ui.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
@@ -29,10 +36,30 @@ object DestinasiEntry : DestinasiNavigasi {
 @Composable
 fun AddBuku(
     navigateBack: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    addBukuViewModel: AddBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) { innerPadding ->
+        EntryBodyBuku(
+            addUIStateBuku = addBukuViewModel.addUIStateBuku,
+            onBukuValueChange = addBukuViewModel::updateAddUIStateBuku,
+            onSaveClickBuku = {
+                coroutineScope.launch {
+                    addBukuViewModel.addBuku()
+                    navigateBack
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
 
 @Composable
