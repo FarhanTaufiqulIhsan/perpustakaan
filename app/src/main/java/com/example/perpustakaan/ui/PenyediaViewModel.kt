279f831fa4179f9e88030f4fde40be2a02a42291
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 08:51:04 +0700

    membuat objek PenyediaViewModel.Factory untuk membuat instance HomeViewModel dari anggotaRepository di aplikasi Perpus

--- app/src/main/java/com/example/perpustakaan/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/PenyediaViewModel.kt
@@ -2,7 +2,19 @@ package com.example.perpustakaan.ui
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.perpustakaan.PerpusAplication
+import com.example.perpustakaan.ui.anggota.HomeViewModel
 
 fun CreationExtras.aplikasiPerpus(): PerpusAplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PerpusAplication)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiPerpus().container.anggotaRepository)
+        }
+    }
+}
