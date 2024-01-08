e2abe3d6a40d812e8f09b40d1399d7fc727e0141
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 18:32:27 +0700

    menambahkan Row untuk menampilkan teks labelResIDAnggota dan itemDetailAnggota

--- app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggota.kt
@@ -1,10 +1,13 @@
 package com.example.perpustakaan.ui.anggota.detailAnggota
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
 object DetailDestinationAnggota : DestinasiNavigasi {
@@ -19,7 +22,13 @@ private fun ItemDetailsRowAnggota(
     labelResIDAnggota: String,
     itemDetailAnggota: String,
     modifier: Modifier = Modifier
-) {}
+) {
+    Row(modifier = modifier) {
+        Text(text = labelResIDAnggota, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetailAnggota, fontWeight = FontWeight.Bold)
+    }
+}
 
 @Composable
 private fun DeleteConfirmationDialog(
