3fd963bfa9ca2c423bd32e50520ccc11549df9f8
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 17:04:23 +0700

    menambahkan class DetailAnggotaViewModel dan parameter nya

--- app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/detailAnggota/DetailAnggotaViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.perpustakaan.ui.anggota.detailAnggota
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.perpustakaan.data.AnggotaRepository
+
+class DetailAnggotaViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: AnggotaRepository
+) : ViewModel() {}
