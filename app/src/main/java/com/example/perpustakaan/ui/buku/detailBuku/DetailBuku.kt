0713bde2b18287025a11e96136aac20b207d7c6e
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 20:02:22 +0700

    Menambahkan object DetailDestinationBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBuku.kt
@@ -1,2 +1,11 @@
 package com.example.perpustakaan.ui.buku.detailBuku
 
+import com.example.perpustakaan.navigation.DestinasiNavigasi
+
+object DetailDestinationBuku : DestinasiNavigasi{
+    override val route = "item_details_buku"
+    override val titleRes = "Detail Anggota"
+    const val bukuId = "itemBukuId"
+    val routeWithArgs = "$route/{$bukuId}"
+
+}
