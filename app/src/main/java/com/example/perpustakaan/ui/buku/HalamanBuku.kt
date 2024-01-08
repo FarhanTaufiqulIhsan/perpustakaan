24d81511a1133bef5a507e4b2cb37d388f4dc0c6
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 13:18:06 +0700

    Menambahkan Column dan permisalan if else pada fun BodyHomeBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBuku.kt
@@ -18,7 +18,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.model.Anggota
 import com.example.perpustakaan.model.Buku
@@ -42,7 +44,25 @@ fun BodyHomeBuku(
     modifier: Modifier = Modifier,
     onBukuClick: (String) -> Unit = {}
 ){
-
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemBuku.isEmpty()){
+            Text(
+                text = "Tidak ada data Buku",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+            ListBuku(
+                itemBuku = itemBuku,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp),
+                onItemClick = { onBukuClick(it.id) }
+            )
+        }
+    }
 }
 @Composable
 fun ListBuku(
