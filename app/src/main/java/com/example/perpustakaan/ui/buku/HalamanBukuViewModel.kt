d8492de6b324345e2ebf9e05d18962117ef9aa2d
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 12:30:21 +0700

    Membuat class HalamanBukuViewModel

--- app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBukuViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/HalamanBukuViewModel.kt
@@ -1,10 +1,38 @@
 package com.example.perpustakaan.ui.buku
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.perpustakaan.data.AnggotaRepository
+import com.example.perpustakaan.data.BukuRepository
 import com.example.perpustakaan.model.Anggota
+import com.example.perpustakaan.ui.HomeUIStateAnggota
+import com.example.perpustakaan.ui.HomeUIStateBuku
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 sealed class BukuUIState {
     data class Success(val buku: Flow<List<Anggota>>) : BukuUIState()
     object Error : BukuUIState()
     object Loading : BukuUIState()
+}
+
+class HalamanBukuViewModel(private val bukuRepository: BukuRepository) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUIState: StateFlow<HomeUIStateBuku> = bukuRepository.getAll()
+        .filterNotNull()
+        .map {
+            HomeUIStateBuku (listBuku = it.toList(), it.size)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUIStateBuku()
+        )
 }
