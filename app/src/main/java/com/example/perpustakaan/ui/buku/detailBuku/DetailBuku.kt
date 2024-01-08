2d46a5df3b3c40f604a40553047ef96fc910ff22
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 20:39:32 +0700

    Menambahkan fun ItemDetailsBuku dengan paramnya, Card, dan column

--- app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBuku.kt
@@ -1,13 +1,22 @@
 package com.example.perpustakaan.ui.buku.detailBuku
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.perpustakaan.model.Buku
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
 object DetailDestinationBuku : DestinasiNavigasi{
@@ -16,6 +25,29 @@ object DetailDestinationBuku : DestinasiNavigasi{
     const val bukuId = "itemBukuId"
     val routeWithArgs = "$route/{$bukuId}"
 
+}
+
+@Composable
+fun ItemDetailsBuku(
+    buku: Buku,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+
+        }
+    }
 }
 
 @Composable
