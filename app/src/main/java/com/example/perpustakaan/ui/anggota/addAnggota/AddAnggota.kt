6328301bcb736f1f8ee30160d3ee962cd4d78c93
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 14:19:14 +0700

    membuat Column pada EntryBodyAnggota dengan FormInputAnggota dan Button untuk menyimpan data

--- app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggota.kt
@@ -3,8 +3,11 @@ package com.example.perpustakaan.ui.anggota.addAnggota
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,14 +29,33 @@ fun EntryBodyAnggota(
     onAnggotaValueChange: (AddEventAnggota) -> Unit,
     onSaveClickAnggota: () -> Unit,
     modifier: Modifier = Modifier
-) {}
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputAnggota(
+            addEventAnggota = addUIStateAnggota.addEventAnggota,
+            onValueChangeAnggota = onAnggotaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        
+        Button(
+            onClick = onSaveClickAnggota,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Submit")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputAnggota(
     addEventAnggota: AddEventAnggota,
     modifier: Modifier = Modifier,
-    onValueChange: (AddEventAnggota) -> Unit = {},
+    onValueChangeAnggota: (AddEventAnggota) -> Unit = {},
     enabled: Boolean = true
 ) {
     Column(
@@ -42,7 +64,7 @@ fun FormInputAnggota(
     ) {
         OutlinedTextField(
             value = addEventAnggota.nama,
-            onValueChange = { onValueChange(addEventAnggota.copy(nama = it)) },
+            onValueChange = { onValueChangeAnggota(addEventAnggota.copy(nama = it)) },
             label = { Text("Nama") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
@@ -51,7 +73,7 @@ fun FormInputAnggota(
 
         OutlinedTextField(
             value = addEventAnggota.alamat,
-            onValueChange = { onValueChange(addEventAnggota.copy(alamat = it)) },
+            onValueChange = { onValueChangeAnggota(addEventAnggota.copy(alamat = it)) },
             label = { Text("Alamat") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
@@ -60,7 +82,7 @@ fun FormInputAnggota(
 
         OutlinedTextField(
             value = addEventAnggota.jk,
-            onValueChange = { onValueChange(addEventAnggota.copy(jk = it)) },
+            onValueChange = { onValueChangeAnggota(addEventAnggota.copy(jk = it)) },
             label = { Text("Jenis Kelamin") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
@@ -69,7 +91,7 @@ fun FormInputAnggota(
 
         OutlinedTextField(
             value = addEventAnggota.nohp,
-            onValueChange = { onValueChange(addEventAnggota.copy(nohp = it)) },
+            onValueChange = { onValueChangeAnggota(addEventAnggota.copy(nohp = it)) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             label = { Text(text = "Nomor Telepon") },
             modifier = Modifier.fillMaxWidth(),
