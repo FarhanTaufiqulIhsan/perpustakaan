561b5b330a4ec81cad5af664cf22f42ba1fea4ac
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 20:18:21 +0700

    Menambahkan fun DeleteConfirmationDialog pada DetailBuku.kt

--- app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBuku.kt
@@ -1,5 +1,6 @@
 package com.example.perpustakaan.ui.buku.detailBuku
 
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
 object DetailDestinationBuku : DestinasiNavigasi{
@@ -7,5 +8,13 @@ object DetailDestinationBuku : DestinasiNavigasi{
     override val titleRes = "Detail Anggota"
     const val bukuId = "itemBukuId"
     val routeWithArgs = "$route/{$bukuId}"
+
+}
+
+private fun DeleteConfirmationDialog(
+    onDeleteConfirmBuku: () -> Unit,
+    onDeletecancelmBuku: () -> Unit,
+    modifier: Modifier = Modifier
+){
 
 }
