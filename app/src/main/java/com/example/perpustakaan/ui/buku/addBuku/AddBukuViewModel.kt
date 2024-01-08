0be1bb5cd8b99a828211b2d4421bb7f201bb01e2
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 14:53:30 +0700

    Menambah suspend fun addBuku untuk menambahkan buku menggunakan data dari state UI AddBukuViewModel

--- app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBukuViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/addBuku/AddBukuViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import com.example.perpustakaan.data.BukuRepository
 import com.example.perpustakaan.ui.AddEventBuku
 import com.example.perpustakaan.ui.AddUIStateBuku
+import com.example.perpustakaan.ui.toBuku
 
 class AddBukuViewModel(private val bukuRepository: BukuRepository) : ViewModel() {
     var addUIStateBuku by mutableStateOf(AddUIStateBuku())
@@ -14,5 +15,9 @@ class AddBukuViewModel(private val bukuRepository: BukuRepository) : ViewModel()
 
     fun updateAddUIStateBuku(addEventBuku: AddEventBuku) {
         addUIStateBuku = AddUIStateBuku(addEventBuku = addEventBuku)
+    }
+
+    suspend fun addBuku() {
+        bukuRepository.save(addUIStateBuku.addEventBuku.toBuku())
     }
 }
