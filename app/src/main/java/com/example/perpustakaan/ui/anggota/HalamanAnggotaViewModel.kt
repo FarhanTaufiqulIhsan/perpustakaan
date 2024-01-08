5328471011a53b2d062d0fb27f9efd442dcb6a7b
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 08:13:48 +0700

    membuat class HomeViewModel dengan repository AnggotaRepository sebagai parameter konstruktor, dan konstanta TIMEOUT_MILLIS sebesar 5,000L

--- app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggotaViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.perpustakaan.ui.anggota
 
+import androidx.lifecycle.ViewModel
+import com.example.perpustakaan.data.AnggotaRepository
 import com.example.perpustakaan.model.Anggota
 import kotlinx.coroutines.flow.Flow
 
@@ -7,4 +9,10 @@ sealed class AnggotaUIState {
     data class Success(val anggota: Flow<List<Anggota>>) : AnggotaUIState()
     object Error : AnggotaUIState()
     object Loading : AnggotaUIState()
+}
+
+class HomeViewModel(private val anggotaRepository: AnggotaRepository) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
