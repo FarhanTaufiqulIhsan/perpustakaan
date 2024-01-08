7dc3943682104d5a2fe8cf43e9719adeb3dcc089
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-08 19:56:26 +0700

    Membuat class DetailBukuViewModel dan paramnya

--- app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBukuViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/detailBuku/DetailBukuViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.perpustakaan.ui.buku.detailBuku
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.perpustakaan.data.BukuRepository
+
+class DetailBukuViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repository: BukuRepository
+) : ViewModel(){
+
+}
