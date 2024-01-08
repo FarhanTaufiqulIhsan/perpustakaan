6f7cf46f7341d6f5c75ec0613bd5de3943e2e8a1
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 11:25:17 +0700

    membuat Scaffold menggunakan nestedScroll dan menampilkan daftar Anggota dengan state dari view model

--- app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,11 +18,15 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
@@ -44,6 +49,19 @@ fun AnggotaScreen(
     viewModel: HalamanAnggotaViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        ) { innerPadding ->
+        val uiStateAnggota by viewModel.homeUIState.collectAsState()
+        BodyHomeAnggota(
+            itemAnggota = uiStateAnggota.listAnggota,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            onAnggotaClick = onDetailClick
+        )
+    }
 }
 
 @Composable
