13c7796359905b3a7ebc688a12772bab93434aba
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 11:29:59 +0700

    membuat FloatingActionButton dengan ikon tambah, memicu navigateToItemEntryAnggota saat diklik, dan menggunakan padding dan bentuk medium

--- app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/HalamanAnggota.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -59,6 +61,18 @@ fun AnggotaScreen(
                 canNavigateBack = false,
                 scrollBehavior = scrollBehavior
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntryAnggota,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ""
+                )
+            }
         }
         ) { innerPadding ->
         val uiStateAnggota by viewModel.homeUIState.collectAsState()
