8ca217e5cd1d4ae10ab920633aa77d18153dea30
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-08 14:11:46 +0700

    membuat FormInputAnggota untuk input data Anggota dengan TextField terstruktur untuk Nama, Alamat, Jenis Kelamin, dan Nomor Telepon

--- app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/addAnggota/AddAnggota.kt
@@ -1,7 +1,16 @@
 package com.example.perpustakaan.ui.anggota.addAnggota
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 import com.example.perpustakaan.ui.AddEventAnggota
 
@@ -10,10 +19,53 @@ object DestinasiEntry : DestinasiNavigasi {
     override val titleRes = "Entry Anggota"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputAnggota(
     addEventAnggota: AddEventAnggota,
     modifier: Modifier = Modifier,
     onValueChange: (AddEventAnggota) -> Unit = {},
     enabled: Boolean = true
-) {}
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = addEventAnggota.nama,
+            onValueChange = { onValueChange(addEventAnggota.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = addEventAnggota.alamat,
+            onValueChange = { onValueChange(addEventAnggota.copy(alamat = it)) },
+            label = { Text("Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = addEventAnggota.jk,
+            onValueChange = { onValueChange(addEventAnggota.copy(jk = it)) },
+            label = { Text("Jenis Kelamin") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = addEventAnggota.nohp,
+            onValueChange = { onValueChange(addEventAnggota.copy(nohp = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = "Nomor Telepon") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
+}
