8157f4da085c583288ea95a894ed5c2e7218f108
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-09 20:31:38 +0700

    membuat objek EditDestinationAnggota untuk rute 'item edit anggota' dengan judul 'Edit Anggota' dan argumen rute anggotaId

--- app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggota.kt
@@ -1,2 +1,10 @@
 package com.example.perpustakaan.ui.anggota.editAnggota
 
+import com.example.perpustakaan.navigation.DestinasiNavigasi
+
+object EditDestinationAnggota : DestinasiNavigasi {
+    override val route = "item edit anggota"
+    override val titleRes ="Edit Anggota"
+    const val anggotaId = "anggotaId"
+    val routeWithArgs = "${EditDestinationAnggota.route}/{$anggotaId}"
+}
