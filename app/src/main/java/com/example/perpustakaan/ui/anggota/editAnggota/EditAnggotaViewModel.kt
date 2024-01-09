7dcb423400196d02eeb0d01c9a86fa7f2dac0d32
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-09 20:39:16 +0700

    menambahkan inisialisasi anggotaUIState dengan data Anggota berdasarkan ID dari repository

--- app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggotaViewModel.kt
@@ -5,8 +5,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perpustakaan.data.AnggotaRepository
 import com.example.perpustakaan.ui.AddUIStateAnggota
+import com.example.perpustakaan.ui.toUIStateAnggota
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class EditAnggotaViewModel(
     savedStateHandle: SavedStateHandle,
@@ -16,4 +21,14 @@ class EditAnggotaViewModel(
         private set
 
     private val anggotaId: String = checkNotNull(savedStateHandle[EditDestinationAnggota.anggotaId])
+
+    init {
+        viewModelScope.launch {
+            anggotaUIState =
+                repository.getAnggotaById(anggotaId)
+                    .filterNotNull()
+                    .first()
+                    .toUIStateAnggota()
+        }
+    }
 }
