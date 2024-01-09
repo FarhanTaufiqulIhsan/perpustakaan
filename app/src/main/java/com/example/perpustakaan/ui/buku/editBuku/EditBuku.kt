d2622aeb01a503fccad2340a9b84beca66ccd4d3
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-09 21:33:03 +0700

    Membuat fun EditScreenBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/editBuku/EditBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/editBuku/EditBuku.kt
@@ -1,10 +1,24 @@
 package com.example.perpustakaan.ui.buku.editBuku
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.navigation.DestinasiNavigasi
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.perpustakaan.ui.PenyediaViewModel
 
 object EditDestinationBuku : DestinasiNavigasi{
     override val route = "item edit buku"
     override val titleRes = "Edit Buku"
     const val bukuId = "bukuId"
     val routeWithArgs = "${EditDestinationBuku.route}/{$bukuId}"
+}
+
+@Composable
+fun EditScreenBuku(
+    navigateBackBuku: () -> Unit,
+    onNavigateUpBuku: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
