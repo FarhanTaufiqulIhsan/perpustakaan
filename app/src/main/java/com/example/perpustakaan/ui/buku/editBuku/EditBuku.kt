8c1c904dcfa0e1cabbac0778b37ff72ce468f731
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-09 21:16:39 +0700

    Menambahkan object EditDestinationBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/editBuku/EditBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/editBuku/EditBuku.kt
@@ -1,2 +1,10 @@
 package com.example.perpustakaan.ui.buku.editBuku
 
+import com.example.perpustakaan.navigation.DestinasiNavigasi
+
+object EditDestinationBuku : DestinasiNavigasi{
+    override val route = "item edit buku"
+    override val titleRes = "Edit Buku"
+    const val bukuId = "bukuId"
+    val routeWithArgs = "${EditDestinationBuku.route}/{$bukuId}"
+}
