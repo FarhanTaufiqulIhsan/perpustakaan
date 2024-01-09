e2324123582a2534ef3e84ee2c827ffd0a9abc2a
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-09 20:25:54 +0700

    menambah variabel anggotaUIState

--- app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggotaViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.perpustakaan.ui.anggota.editAnggota
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.perpustakaan.data.AnggotaRepository
+import com.example.perpustakaan.ui.AddUIStateAnggota
 
 class EditAnggotaViewModel(
     savedStateHandle: SavedStateHandle,
     private val repository: AnggotaRepository
-) {}
+) : ViewModel() {
+    var anggotaUIState by mutableStateOf(AddUIStateAnggota())
+        private set
+}
