4f579a0d4ab66eb8e2b2f812a11d1fc823849d6c
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-09 20:14:28 +0700

    membuat class EditAnggotaViewModel dan parameter nya

---
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggotaViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.perpustakaan.ui.anggota.editAnggota
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.perpustakaan.data.AnggotaRepository
+
+class EditAnggotaViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: AnggotaRepository
+) {}
