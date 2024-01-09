8376d48ed0eb8fe2d3b1c6c116433dfb41b09487
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-09 20:54:38 +0700

    menambahkan variabel coroutineScope

--- app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggota.kt
+++ app/src/main/java/com/example/perpustakaan/ui/anggota/editAnggota/EditAnggota.kt
@@ -1,6 +1,7 @@
 package com.example.perpustakaan.ui.anggota.editAnggota
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
@@ -16,4 +17,6 @@ fun EditScreenAnggota(
     navigateBackAnggota: () -> Unit,
     onNavigateUpAnggota: () -> Unit,
     modifier: Modifier = Modifier,
-) {}
+) {
+    val coroutineScope = rememberCoroutineScope()
+}
