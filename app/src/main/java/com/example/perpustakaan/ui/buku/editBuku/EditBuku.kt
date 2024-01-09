50a55445cbe7f1331cbf579d53a32166b16c6c2c
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-09 21:40:11 +0700

    Membuat val coroutinescope, memanggil file EntryBodyBuku

--- app/src/main/java/com/example/perpustakaan/ui/buku/editBuku/EditBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/buku/editBuku/EditBuku.kt
@@ -1,10 +1,19 @@
 package com.example.perpustakaan.ui.buku.editBuku
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.perpustakaan.ui.PenyediaViewModel
+import com.example.perpustakaan.ui.buku.addBuku.EntryBodyBuku
+import kotlinx.coroutines.launch
 
 object EditDestinationBuku : DestinasiNavigasi{
     override val route = "item edit buku"
@@ -13,6 +22,7 @@ object EditDestinationBuku : DestinasiNavigasi{
     val routeWithArgs = "${EditDestinationBuku.route}/{$bukuId}"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditScreenBuku(
     navigateBackBuku: () -> Unit,
@@ -20,5 +30,21 @@ fun EditScreenBuku(
     modifier: Modifier = Modifier,
     viewModel: EditBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val coroutineScope = rememberCoroutineScope()
 
+    Scaffold() { innerPadding ->
+        EntryBodyBuku(
+            addUIStateBuku = viewModel.bukuUIState,
+            onBukuValueChange = viewModel::updateUIStateBuku,
+            onSaveClickBuku = { coroutineScope.launch {
+                viewModel.updateBuku()
+                navigateBackBuku()
+            }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
