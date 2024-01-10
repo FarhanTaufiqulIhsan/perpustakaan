ad35872624193a544e67c821e8a8ffc01b7397f8
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-10 12:09:12 +0700

    membuat variabel scrollBehavior

--- app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjaman.kt
+++ app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjaman.kt
@@ -13,9 +13,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,12 +31,15 @@ object DestinasiHomePeminjaman : DestinasiNavigasi {
     override val titleRes = "Peminjaman"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PeminjamanScreen(
     navigateToItemEntryPeminjaman: () -> Unit,
     modifier: Modifier = Modifier,
     onDetailClickPeminjaman: (String) -> Unit = {}
-) {}
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
 
 @Composable
 fun BodyHomePeminjaman(
