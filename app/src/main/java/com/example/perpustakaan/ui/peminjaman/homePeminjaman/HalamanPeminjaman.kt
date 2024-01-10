3b31cdcb9b6aab7ff314e6add3ce28979d9e60a1
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-10 11:58:08 +0700

    membuat Card pada function DataPeminjaman

--- app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjaman.kt
+++ app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjaman.kt
@@ -1,7 +1,22 @@
 package com.example.perpustakaan.ui.peminjaman.homePeminjaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.model.Peminjaman
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
@@ -14,4 +29,50 @@ object DestinasiHomePeminjaman : DestinasiNavigasi {
 fun DataPeminjaman(
     peminjaman: Peminjaman,
     modifier: Modifier = Modifier
-) {}
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = peminjaman.buku.judul,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = null,
+                )
+                Text(
+                    text = peminjaman.tanggalPinjam.toString(),
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = peminjaman.anggota.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = null,
+                )
+                Text(
+                    text = peminjaman.tanggalKembali.toString(),
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+    }
+}
