453ab096b79e08fc29075722ddd5b71c65c0bf7e
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-10 11:46:41 +0700

    menambahkan variabel homeUIStatePeminjaman

--- app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjamanViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjamanViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.perpustakaan.ui.peminjaman.homePeminjaman
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perpustakaan.data.PeminjamanRepository
 import com.example.perpustakaan.model.Peminjaman
+import com.example.perpustakaan.ui.HomeUIStateAnggota
+import com.example.perpustakaan.ui.HomeUIStatePeminjaman
+import com.example.perpustakaan.ui.anggota.homeAnggota.HalamanAnggotaViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 sealed class PeminjamanUIState {
     data class Success(val peminjaman: Flow<List<Peminjaman>>) : PeminjamanUIState()
@@ -15,4 +24,15 @@ class HalamanPeminjamanViewModel(private val peminjamanRepository: PeminjamanRep
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    val homeUIStatePeminjaman: StateFlow<HomeUIStatePeminjaman> = peminjamanRepository.getAll()
+        .filterNotNull()
+        .map {
+            HomeUIStatePeminjaman (listPeminjaman = it.toList(), it.size)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(HalamanPeminjamanViewModel.TIMEOUT_MILLIS),
+            initialValue = HomeUIStatePeminjaman()
+        )
 }
