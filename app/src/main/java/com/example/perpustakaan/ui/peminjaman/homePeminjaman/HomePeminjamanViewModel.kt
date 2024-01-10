a8833f1e11ce363f9f12adefbd3310c33887571e
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-10 11:22:53 +0700

    menambahkan sealed class PeminjamanUIState

--- app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HomePeminjamanViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HomePeminjamanViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.perpustakaan.ui.peminjaman.homePeminjaman
 
+import com.example.perpustakaan.model.Peminjaman
+import kotlinx.coroutines.flow.Flow
+
+sealed class PeminjamanUIState {
+    data class Success(val peminjaman: Flow<List<Peminjaman>>) : PeminjamanUIState()
+    object Error : PeminjamanUIState()
+    object Loading : PeminjamanUIState()
+}
