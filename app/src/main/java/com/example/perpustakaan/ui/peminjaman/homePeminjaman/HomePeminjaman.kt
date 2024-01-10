3594094e30ff7bbb58b621488f956960df66d453
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-10 11:03:36 +0700

    menambahkan object DestinasiHomePeminjaman

---
+++ app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HomePeminjaman.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.peminjaman.homePeminjaman
+
+import com.example.perpustakaan.navigation.DestinasiNavigasi
+
+object DestinasiHomePeminjaman : DestinasiNavigasi {
+    override val route = "home peminjaman"
+    override val titleRes = "Peminjaman"
+}
