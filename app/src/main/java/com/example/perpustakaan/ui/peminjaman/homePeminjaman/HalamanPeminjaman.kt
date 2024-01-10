8e486d6c791a0c707b5fa86bd2355d0579b9b3a4
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2024-01-10 11:48:47 +0700

    membuat function DataPeminjaman dan parameter nya

--- app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjaman.kt
+++ app/src/main/java/com/example/perpustakaan/ui/peminjaman/homePeminjaman/HalamanPeminjaman.kt
@@ -1,8 +1,17 @@
 package com.example.perpustakaan.ui.peminjaman.homePeminjaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.perpustakaan.model.Peminjaman
 import com.example.perpustakaan.navigation.DestinasiNavigasi
 
 object DestinasiHomePeminjaman : DestinasiNavigasi {
     override val route = "home peminjaman"
     override val titleRes = "Peminjaman"
 }
+
+@Composable
+fun DataPeminjaman(
+    peminjaman: Peminjaman,
+    modifier: Modifier = Modifier
+) {}
