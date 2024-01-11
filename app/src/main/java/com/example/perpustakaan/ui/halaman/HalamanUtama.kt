c1017d29db1e0808c8168304d2b8c1dbba67a8cf
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2024-01-12 01:20:52 +0700

    Mendesign HalamanUtama

--- app/src/main/java/com/example/perpustakaan/ui/halaman/HalamanUtama.kt
+++ app/src/main/java/com/example/perpustakaan/ui/halaman/HalamanUtama.kt
@@ -1,19 +1,26 @@
 package com.example.perpustakaan.ui.halaman
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -32,6 +39,20 @@ fun HalamanUtama(
     onAnggotaClick: () -> Unit,
     onPeminjamanClick: () -> Unit
 ) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.background2),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -42,7 +63,7 @@ fun HalamanUtama(
     ) {
         Text(
             text = stringResource(id = R.string.perpus),
-            color = Color.DarkGray,
+            color = Color.White,
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(bottom = 150.dp, top = 100.dp)
@@ -56,6 +77,7 @@ fun HalamanUtama(
         ) {
             Button(
                 onClick = onBukuClick,
+                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.brown)),
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxHeight()
@@ -65,6 +87,7 @@ fun HalamanUtama(
 
             Button(
                 onClick = onAnggotaClick,
+                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.brown)),
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxHeight()
@@ -77,6 +100,7 @@ fun HalamanUtama(
 
         Button(
             onClick = onPeminjamanClick,
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.brown)),
             modifier = Modifier
                 .fillMaxWidth()
                 .height(72.dp)
